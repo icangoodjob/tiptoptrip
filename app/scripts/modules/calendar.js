e3a0b26359fd062c202792b8372ff6d0c86ebedb
Alexey <lxmlshv97@mail.ru>
2025-12-06 15:06:02 +0300

    styles and new logic for calendar

--- app/scripts/modules/calendar.js
+++ app/scripts/modules/calendar.js
@@ -7,52 +7,48 @@ const calendarOptions = (device) => {
     return {
       layouts: {
         multiple: `
-          <div class="vc-container">
-            <div class="vc-choices" data-vc-choices>
-                <div class="vc-choices__item">
-                  <div class="vc-choices__caption">Заезд</div>
-                  <div class="vc-choices__value" id="vc-value-from"></div>
-                </div>
-                <div class="vc-choices__item">
-                  <div class="vc-choices__caption">Выезд</div>
-                  <div class="vc-choices__value" id="vc-value-to"></div>
-                </div>
+          <div class="vc-choices" data-vc-choices>
+            <div class="vc-choices__item">
+              <div class="vc-choices__caption">Заезд</div>
+              <div class="vc-choices__value" id="vc-value-from"></div>
             </div>
-            <div class="vc-body">
-              <div class="vc-grid" data-vc="grid">
-                <#Multiple>
-                  <div class="vc-column" data-vc="column" role="region">
-                    <div class="vc-header" data-vc="header">
-                      <div class="vc-controls" data-vc="controls" role="toolbar" aria-label="Навигация">
-                        <#ArrowPrev [month] />
-                        <#ArrowNext [month] />
-                      </div>
-                      <div class="vc-header-content" data-vc-header="content">
-                        <#Month />
-                        <#Year />
-                      </div>
-                    </div>
-                    <div class="vc-wrapper" data-vc="wrapper">
-                      <#WeekNumbers />
-                      <div class="vc-content" data-vc="content">
-                        <#Week />
-                        <#Dates />
-                      </div>
-                    </div>
-                  </div>
-                <#/Multiple>
-                <#DateRangeTooltip />
-              </div>
-            </div>
-            <div class="vc-footer">
-              <div class="vc-footer__buttons">
-                <button class="vc-button vc-button-reset btn btn--border" id="vc-button-reset" type="button">Сбросить</button>
-                <button class="vc-button vc-button-apply btn btn--primary" id="vc-button-apply" type="button">Применить</button>
-              </div>
+            <div class="vc-choices__item">
+              <div class="vc-choices__caption">Выезд</div>
+              <div class="vc-choices__value" id="vc-value-to"></div>
             </div>
           </div>
-        <#ControlTime />
-      `,
+          <div class="vc-controls" data-vc="controls" role="toolbar" aria-label="">
+            <#ArrowPrev [month] />
+            <#ArrowNext [month] />
+          </div>
+          <div class="vc-grid" data-vc="grid">
+            <#Multiple>
+              <div class="vc-column" data-vc="column" role="region">
+                <div class="vc-header" data-vc="header">
+                  <div class="vc-header-content" data-vc-header="content">
+                    <#Month />
+                    <#Year />
+                  </div>
+                </div>
+                <div class="vc-wrapper" data-vc="wrapper">
+                  <#WeekNumbers />
+                  <div class="" data-vc="content">
+                    <#Week />
+                    <#Dates />
+                  </div>
+                </div>
+              </div>
+            <#/Multiple>
+            <#DateRangeTooltip />
+          </div>
+          <div class="vc-footer">
+            <div class="vc-footer__buttons">
+              <button class="vc-button vc-button-reset btn btn--border" id="vc-button-reset" type="button">Сбросить</button>
+              <button class="vc-button vc-button-apply btn btn--primary" id="vc-button-apply" type="button">Применить</button>
+            </div>
+          </div>
+          <#ControlTime />
+        `,
       },
     };
   } else {
@@ -94,7 +90,7 @@ const calendarOptions = (device) => {
             </div>
             <div class="vc-footer">
               <div class="vc-footer__buttons">
-                <button class="vc-button vc-button-reset btn btn--border" id="vc-button-reset" type="button">Сбросить</button>
+                <button class="vc-button vc-button-reset btn btn--border" id="vc-button-reset" data-vc-btn-reset type="button">Сбросить</button>
                 <button class="vc-button vc-button-apply btn btn--primary" id="vc-button-apply" type="button">Применить</button>
               </div>
             </div>
@@ -160,31 +156,45 @@ function resetValues() {
   if (calendarValueFrom && calendarValueTo) {
     calendarValueFrom.textContent = calendarValueTo.textContent = "";
   }
+  transformationChoices([]);
+  stateActionsButtons([]);
 }
 
 function transformationChoices(arrDates) {
   const calendarChoices = document.querySelector("[data-vc-choices]");
   if (calendarChoices) {
     arrDates.length > 0 ? calendarChoices.classList.add("active") : calendarChoices.classList.remove("active");
-    console.log("calendarChoices", calendarChoices);
+  }
+}
+
+function stateActionsButtons(arrDates) {
+  const vcButtonReset = document.getElementById("vc-button-reset");
+
+  const vcButtonApply = document.getElementById("vc-button-apply");
+
+  if (vcButtonApply && vcButtonReset) {
+    arrDates.length > 0 ? vcButtonReset.removeAttribute("disabled") : vcButtonReset.setAttribute("disabled", true);
+    arrDates.length > 0 ? vcButtonApply.removeAttribute("disabled") : vcButtonApply.setAttribute("disabled", true);
   }
 }
 
 const options = {
+  // selectedTheme: "dark",
   locale: "ru",
   type: "multiple",
   displayMonthsCount: 2,
-  monthsToSwitch: 2,
   displayDatesOutside: false,
   disableDatesPast: true,
   enableEdgeDatesOnly: true,
   selectionDatesMode: "multiple-ranged",
+  // monthsToSwitch: 2,
   ...opts,
   onClickDate(self) {
     const selectedDates = self.context.selectedDates;
     console.log(selectedDates);
     setDateValues(selectedDates);
     transformationChoices(selectedDates);
+    stateActionsButtons(selectedDates);
   },
 };
 
@@ -204,9 +214,22 @@ function showCalendar(button) {
   // Запоминаем текущую кнопку
   currentButton = button;
 
+  // Находим родительский .custom-select
+  const customSelect = button.closest(".custom-select");
+  if (!customSelect) return;
+
+  // Проверяем, есть ли уже контейнер
+  let calendarContainer = customSelect.querySelector(".calendar-container");
+
+  if (calendarContainer) {
+    calendarContainer.appendChild(calendarPopup);
+  }
+
   if (!currentCalendar) {
     currentCalendar = new Calendar(calendarPopup, options);
     currentCalendar.init();
+
+    stateActionsButtons(currentCalendar.context.selectedDates);
   } else {
     // Не обновляем, если нужно сохранить выделение у выбранного диапазона дат
     // currentCalendar.update();
@@ -214,9 +237,8 @@ function showCalendar(button) {
 
   // Позиционируем относительно кнопки
   const rect = button.getBoundingClientRect();
-
-  calendarPopup.style.top = `${rect.bottom + window.scrollY + 5}px`;
-  calendarPopup.style.left = `${rect.left + window.scrollX}px`;
+  // calendarPopup.style.top = `${rect.bottom + window.scrollY + 5}px`;
+  // calendarPopup.style.left = `${rect.left + window.scrollX}px`;
 
   calendarPopup.style.display = "block";
 
@@ -225,7 +247,11 @@ function showCalendar(button) {
   const vcButtonApply = document.getElementById("vc-button-apply");
 
   if (vcButtonReset) {
-    vcButtonReset.addEventListener("click", () => {
+    vcButtonReset.addEventListener("click", (e) => {
+      const vcDates = e.target.closest("#calendar-popup").querySelectorAll("[data-vc-date]");
+      if (vcDates.length > 0) {
+        vcDates.forEach((date) => date.removeAttribute("data-vc-date-selected"));
+      }
       currentCalendar.context.selectedDates = [];
       resetValues();
     });
@@ -259,8 +285,7 @@ const buttons = document.querySelectorAll(".date-picker-button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    // e.stopPropagation();
-
+    e.stopPropagation();
     // Если календарь уже открыт для этой кнопки - закрываем
     if (currentButton === button && calendarPopup?.style.display === "block") {
       hideCalendar();
@@ -273,7 +298,10 @@ buttons.forEach((button) => {
 // Обработчик клика вне календаря
 function handleClickOutside(event) {
   const target = event.target;
-  if (calendarPopup && !calendarPopup.contains(target) && currentButton !== target && !currentButton?.contains(target)) {
+  // Проверяем, является ли клик частью календаря
+  const isCalendarClick = !target.closest(".vc-month") && !target.closest(".vc-year") && !target.closest(".vc-grid");
+  // Проверяем, является ли клик кнопкой открытия календаря
+  if (calendarPopup && !calendarPopup.contains(target) && currentButton !== target && !currentButton?.contains(target) && isCalendarClick) {
     hideCalendar();
   }
 }
